5669895653a72011bab2896d5b176704e44ecd19
iadfoifoqwefqf <anandavikisurvey@gmail.com>
2025-08-13 05:49:08 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,20 +1,11 @@
 terraform {
-  required_providers {
-    local = {
-      source  = "hashicorp/local"
-      version = "2.4.0"
-    }
-  }
-  required_version = ">= 1.0"
+  required_version = ">= 1.0.0"
 }
 
-provider "local" {}
-
-resource "local_file" "test_file" {
-  content  = "Terraform test file from workflow-terraform repo"
-  filename = "${path.module}/tf_output/test_file.txt"
+provider "local" {
 }
 
-output "file_path" {
-  value = local_file.test_file.filename
+resource "local_file" "example" {
+  content  = "Hello from Terraform at ${timestamp()}"
+  filename = "${path.module}/output.txt"
 }
